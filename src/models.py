bd70494e723b4f118589593c195cd69e8b860367
pinarezgicol <pinar.col@metu.edu.tr>
2023-02-03 15:41:33 +0300

    another resnet model with dropout is implemented

--- src/models.py
+++ src/models.py
@@ -4,49 +4,89 @@ from simclr.modules import get_resnet
 import torch
 import torch.nn as nn
 
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU())
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(out_channels))
-        self.downsample = downsample
+
+class Block(nn.Module):
+    def __init__(self, in_channels=3, out_channels=10, identity_downsample=None, stride=1):
+        super(Block, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.5)
-        self.out_channels = out_channels
+        self.identity_downsample = identity_downsample
 
     def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.conv2(out)
-        if self.downsample:
-            residual = self.downsample(x)
-        out += residual
-        out = self.relu(out)
-        out = self.dropout(out)
-        return out
+        identity = x
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = nn.Dropout(0.5)(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+        x += identity
+        x = self.relu(x)
+        x = nn.Dropout(0.5)(x)
+        return x
 
 
-class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=10):
-        super(ResNet, self).__init__()
-        self.inplanes = 64
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Dropout(0.5))
+class ResNet_18(nn.Module):
+
+    def __init__(self, image_channels, num_classes):
+
+        super(ResNet_18, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer0 = self._make_layer(block, 64, layers[0], stride=1)
-        self.layer1 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer2 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer3 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7, stride=1)
+
+        # resnet layers
+        self.layer1 = self.__make_layer(64, 64, stride=1)
+        self.layer2 = self.__make_layer(64, 128, stride=2)
+        self.layer3 = self.__make_layer(128, 256, stride=2)
+        self.layer4 = self.__make_layer(256, 512, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512, num_classes)
+
+    def __make_layer(self, in_channels, out_channels, stride):
+
+        identity_downsample = None
+        if stride != 1:
+            identity_downsample = self.identity_downsample(in_channels, out_channels)
+
+        return nn.Sequential(
+            Block(in_channels, out_channels, identity_downsample=identity_downsample, stride=stride),
+            Block(out_channels, out_channels)
+        )
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+
+    def identity_downsample(self, in_channels, out_channels):
+
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(out_channels)
+        )
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
@@ -107,7 +147,7 @@ def load_model(classifier_model, dataset="CIFAR10"):
     if classifier_model == "resnet18":
         model = get_resnet(classifier_model, pretrained=False)
     else:
-        model = ResNet(ResidualBlock, [2, 2, 2, 2])
+        model = ResNet_18()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     model = model.to(device)
--- src/utils.py
+++ src/utils.py
@@ -13,26 +13,26 @@ def load_data(dataset, dataset_dir="./datasets"):
             dataset_dir,
             split="train",
             download=True,
-            transform=TransformsSimCLR(size=224).test_transform,
+            transform=TransformsSimCLR(size=96).test_transform,
         )
         test_dataset = torchvision.datasets.STL10(
             dataset_dir,
             split="test",
             download=True,
-            transform=TransformsSimCLR(size=224).test_transform,
+            transform=TransformsSimCLR(size=96).test_transform,
         )
     elif dataset == "CIFAR10":
         train_dataset = torchvision.datasets.CIFAR10(
             dataset_dir,
             train=True,
             download=True,
-            transform=TransformsSimCLR(size=224).test_transform,
+            transform=TransformsSimCLR(size=32).test_transform,
         )
         test_dataset = torchvision.datasets.CIFAR10(
             dataset_dir,
             train=False,
             download=True,
-            transform=TransformsSimCLR(size=224).test_transform,
+            transform=TransformsSimCLR(size=32).test_transform,
         )
     else:
         raise NotImplementedError
